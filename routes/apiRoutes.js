68cf6e211aa211cf0bb5eb941ffd494c0c7b6148
Maya Stucky <mayaatstucky@gmail.com>
2020-07-27 17:34:22 -0400

    fiddled with routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,6 +28,7 @@ module.exports = function (app) {
       });
   });
   //does the same thing as above but for the range
+  //this makes the stats page work
   app.get("/api/workouts/range", function (req, res) {
     db.Workout.find({})
       .then(function (data) {
@@ -38,17 +39,7 @@ module.exports = function (app) {
       });
   });
 
-  app.post("/api/workouts/range", function (req, res) {
-    db.Workout.create({})
-      .then(function (data) {
-        res.json(data);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
-  });
 
-  
   //documentation for findByIdAndUpdate:
   //https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
   app.put("/api/workouts/:id", function (req, res) {
@@ -57,9 +48,11 @@ module.exports = function (app) {
       { _id: req.params.id },
       //now we need to push the new exercise to the exercises array
       //we also need to add/increment the duration that the user input into the correct field so it doesn't stay at 0
-      //in other words, in the model, the totalDuration field gets incremented 
-      { $push: { exercises: req.body }, $inc: {totalDuration : req.body.duration} }, 
-
+      //in other words, in the model, the totalDuration field gets incremented
+      {
+        $push: { exercises: req.body },
+        $inc: { totalDuration: req.body.duration },
+      }
     )
       .then(function (data) {
         res.json(data);
